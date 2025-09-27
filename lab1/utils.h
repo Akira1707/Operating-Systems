d1a31572f5d92233ddc93b5b4cd1385391381e3e
Akira1707 <thuhoai20021707@gmail.com>
2025-09-28 01:42:02 +0300

    Delete

--- lab1/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <syslog.h>
-#include <cstdarg>
-#include <chrono>
-#include <ctime>
-
-std::string timestamp_now();
-void safe_syslog(int priority, const char* fmt, ...);
